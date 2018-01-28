43f27118703e0762c926017d72a43d1641105276
Thomas Pethick <tmpethick@gmail.com>
2018-01-28 16:26:08 +0100

    Adds source

--- src/Foldable.hs
+++ src/Foldable.hs
@@ -6,6 +6,7 @@ import qualified Data.Foldable as F
 import qualified Data.Traversable
 import Data.Functor.Foldable
 
+-- Pulled from https://hackage.haskell.org/package/fixplate-0.1.7/docs/src/Data-Generics-Fixplate-Morphisms.html#cataM
 cataM
   :: (Recursive t, Traversable (Base t), Monad m)
   => (Base t a -> m a) -- ^ a monadic (Base t)-algebra
